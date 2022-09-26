50294379b682ef1638dd5daae5fc731f4889add9
shenlilin <shenlilin@wingtech.com>
2022-09-26 17:11:23 +0800

    add proj files

--- Service/Services/PetCategoryServices/PetCategoryManageService.cs
+++ Service/Services/PetCategoryServices/PetCategoryManageService.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess.Repository.File;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Service.Models.CommonModel;
 
